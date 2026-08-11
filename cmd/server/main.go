d9ef296cadde5c00d17b5cc55188c58c642d4529
jaisondavid-m <jaisondavidm.cs25@bitsathy.ac.in>
2026-08-11 21:32:02 +0530

    merger feature/webhook

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,9 +5,14 @@ import (
 	"os"
 
 	"iTrigger/internal/server"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load environment variables from .env file if present
+	_ = godotenv.Load()
+
 	secret := os.Getenv("GITHUB_WEBHOOK_SECRET")
 	if secret == "" {
 		log.Fatal("GITHUB_WEBHOOK_SECRET must be set")
